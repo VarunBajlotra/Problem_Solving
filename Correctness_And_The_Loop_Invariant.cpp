cb8b1039421fbd86e6dfdd28fa1af00f8882aeb5
Varun Bajlotra <42884780+VarunBajlotra@users.noreply.github.com>
2019-04-11 16:39:10 +0530

    Create Correctness_And_The_Loop_Invariant.cpp
    Question Link : https://www.hackerrank.com/challenges/correctness-invariant/problem

---
+++ Correctness_And_The_Loop_Invariant.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{int n,arr[1005];
+ cin>>n;
+ for(int i=0;i<n;i++)
+    cin>>arr[i];
+ for(int i=0;i<n-1;i++)
+    {int j=i+1;
+     int num=arr[j];
+     while(num<arr[j-1]&&j>0)
+        {arr[j]=arr[j-1];
+         j--;
+        }
+     arr[j]=num;
+    }
+ for(int k=0;k<n;k++)
+        cout<<arr[k]<<" ";
+     cout<<endl;
+ return 0;
+}
