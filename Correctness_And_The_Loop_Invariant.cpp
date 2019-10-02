9f7d2395bd7a951319dd17fb26942e37c3bcd8ef
Achint <42889869+achint740@users.noreply.github.com>
2019-10-02 17:59:37 +0530

    Update Correctness_And_The_Loop_Invariant.cpp

--- Correctness_And_The_Loop_Invariant.cpp
+++ Correctness_And_The_Loop_Invariant.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
-{int n,arr[1005];
+{
+ int n,arr[10000];
  cin>>n;
- for(int i=0;i<n;i++)
+ for(int i=0;i<n;i++){
     cin>>arr[i];
- for(int i=0;i<n-1;i++)
-    {int j=i+1;
+ }
+ for(int i=0;i<n-1;i++){
+     int j=i+1;
      int num=arr[j];
      while(num<arr[j-1]&&j>0)
         {arr[j]=arr[j-1];
@@ -19,5 +20,4 @@ int main()
  for(int k=0;k<n;k++)
         cout<<arr[k]<<" ";
      cout<<endl;
- return 0;
 }
