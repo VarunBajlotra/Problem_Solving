831a122c560c2f8df8abce5829bd26ff92fc8779
Kashavi Sharma <52310294+kashavi@users.noreply.github.com>
2020-10-01 23:27:08 +0530

    Update Funny_String.cpp

--- Funny_String.cpp
+++ Funny_String.cpp
@@ -1,30 +1,26 @@
-#include<iostream>
-#include<cstdlib>
-#include<cstring>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{int t;
+int main(){
+ int t;
  cin>>t;
- while(t--)
-    {char str1[10005],str2[10005];
+ while(t--){
+     string str1,str2="";
      cin>>str1;
-     int len=strlen(str1),dif1,dif2,count=0,c=0;
-     for(int i=len-1;i>=0;i--)
-        {str2[c++]=str1[i];
-        }
-     str2[c]='\0';
+     int len=str1.length(),count=0,c=0;
+     for(int i=len-1;i>=0;i--){
+         str2+=str1[i];
+     }
      for(int i=0;i<len-1;i++)
-        {dif1=abs(str1[i]-str1[i+1]);
-         dif2=abs(str2[i]-str2[i+1]);
+        {int dif1=abs(str1[i]-str1[i+1]);
+         int dif2=abs(str2[i]-str2[i+1]);
          if(dif1==dif2)
             count++;
         }
      if(count==len-1)
-        cout<<"Funny\n";
+        cout<<"Funny\n";//Hehe
      else
-        cout<<"Not Funny\n";
+        cout<<"Not Funny\n";//No hehe
     }
  return 0;
 }
